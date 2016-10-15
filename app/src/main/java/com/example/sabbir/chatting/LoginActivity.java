b1b863d54cdee41177ed476105874b415efc8080
sabbir <sahimelhimu@gmail.com>
2016-10-15 23:25:18 +0600

    added CommonActivity and LoginActivity methods

--- app/src/main/java/com/example/sabbir/chatting/LoginActivity.java
+++ app/src/main/java/com/example/sabbir/chatting/LoginActivity.java
@@ -1,13 +1,121 @@
 package com.example.sabbir.chatting;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.sabbir.chatting.model.User;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginFragment;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.TwitterAuthCredential;
+import com.google.firebase.crash.FirebaseCrash;
+
+public class LoginActivity extends CommonActivity implements GoogleApiClient.OnConnectionFailedListener{
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+
+    private User user;
+    private CallbackManager callbackManager;
+    private GoogleApiClient googleApiClient;
+    private TwitterAuthCredential twitterAuthCredential;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+//FACEBOOK
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager=CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                accessFacebookLoginData(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                FirebaseCrash.report(error);
+                showSnackbar(error.getMessage());
+
+            }
+        });
+
+
+        //GOOGLE SIGN IN
+        GoogleSignInOptions googleSignInOptions=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("312301797686-1bkt0nbecnbctpfoflanjr3sp4fi0aec.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        googleApiClient=new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,googleSignInOptions)
+                .build();
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        listener=getFirebaseAuthResultHandler();
+        initViews();
+        initUser();
+    }
+
+    private FirebaseAuth.AuthStateListener getFirebaseAuthResultHandler() {
+        FirebaseAuth.AuthStateListener stateListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }
+        };
+        return stateListener;
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    private void accessFacebookLoginData(AccessToken accessToken) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initUser() {
+
     }
 }
